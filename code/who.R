f57b5fe20cce69f46f3abb21a2bd686f6ca6079f
weiyuet <weiyuet@gmail.com>
2022-10-06 05:14:11 +0800

    Edited vaccination rates plot

--- code/who.R
+++ code/who.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(janitor)
 library(scales)
 library(ggsci)
+library(patchwork)
 
 # Load data
 vaccination_data <- read_csv("data/who/vaccination-data.csv")
@@ -10,7 +11,7 @@ vaccination_data <- read_csv("data/who/vaccination-data.csv")
 vaccination_data <- vaccination_data %>% clean_names()
 
 # Which countries have the highest vaccination rates?
-vaccination_data %>%
+p1 <- vaccination_data %>%
   select(country, who_region, persons_fully_vaccinated_per100) %>% 
   mutate(country = fct_reorder(country, persons_fully_vaccinated_per100)) %>%
   slice_max(n = 40, order_by = persons_fully_vaccinated_per100) %>% 
@@ -25,20 +26,17 @@ vaccination_data %>%
              size = 3) +
   scale_x_continuous(expand = c(0, 0),
                      breaks = seq(0, 120, 20)) +
-  scale_fill_nejm() +
+  scale_fill_npg() +
   theme_classic() +
   theme(axis.text.y = element_blank(),
-        axis.ticks.y = element_blank()) +
+        axis.ticks.y = element_blank(),
+        legend.position = "bottom") +
   labs(x = "", y = "",
-       fill = "WHO Region",
-       title = "Countries with Highest COVID-19 Vaccination Rates",
-       subtitle = "Persons Fully Vaccinated per 100 Population",
-       caption = "Data: WHO | Graphic: @weiyuet")
-
-ggsave("figures/highest-vaccination-rates.png", width = 5, height = 6)
+       fill = "",
+       title = "Highest Vaccination Rates")
 
 # Which countries have the lowest vaccination rates?
-vaccination_data %>%
+p2 <- vaccination_data %>%
   select(country, who_region, persons_fully_vaccinated_per100) %>% 
   mutate(country = fct_reorder(country, persons_fully_vaccinated_per100)) %>%
   slice_min(n = 40, order_by = persons_fully_vaccinated_per100) %>% 
@@ -53,14 +51,19 @@ vaccination_data %>%
              size = 3) +
   scale_x_continuous(expand = c(0, 0),
                      breaks = seq(0, 25, 5)) +
-  scale_fill_nejm() +
+  scale_fill_npg() +
   theme_classic() +
   theme(axis.text.y = element_blank(),
-        axis.ticks.y = element_blank()) +
+        axis.ticks.y = element_blank(),
+        legend.position = "bottom") +
+  guides(fill = guide_legend(nrow = 2)) +
   labs(x = "", y = "",
-       fill = "WHO Region",
-       title = "Countries with Lowest COVID-19 Vaccination Rates",
-       subtitle = "Persons Fully Vaccinated per 100 Population",
-       caption = "Data: WHO | Graphic: @weiyuet")
+       fill = "",
+       title = "Lowest Vaccination Rates")
 
-ggsave("figures/lowest-vaccination-rates.png", width = 5, height = 6)
+p <- p1 | p2
+
+p + plot_annotation(title = "Persons Fully Vaccinated per 100 Population",
+                    caption = "Data: WHO | Graphic: @weiyuet")
+
+ggsave("figures/country-vaccination-rates.png", width = 8, height = 8)
